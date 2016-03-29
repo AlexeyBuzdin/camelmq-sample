92b37bb5944948971c7dbeb29a6223d9254a8c59
Alexey Buzdin <alexey.buzdin@ctco.lv>
2016-03-30 00:49:17 +0300

    Put RabbitTemplate into .xml config

--- src/main/java/com/github/alexeybuzdin/capitalize/CapitalizeConfiguration.java
+++ src/main/java/com/github/alexeybuzdin/capitalize/CapitalizeConfiguration.java
@@ -1,18 +1,9 @@
 package com.github.alexeybuzdin.capitalize;
 
-import org.springframework.amqp.rabbit.connection.*;
-import org.springframework.amqp.rabbit.core.*;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @ImportResource({"classpath:context.xml"})
 public class CapitalizeConfiguration {
 
-	@Bean
-	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-		RabbitTemplate template = new RabbitTemplate(connectionFactory);
-		template.setExchange("capitalize");
-		template.setRoutingKey("capitalize.out");
-		return template;
-	}
 }
--- src/main/java/com/github/alexeybuzdin/capitalize/CapitalizeListener.java
+++ src/main/java/com/github/alexeybuzdin/capitalize/CapitalizeListener.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.*;
 public class CapitalizeListener {
 
     @Autowired
-    RabbitTemplate template;
+    RabbitTemplate capitalizeTemplate;
 
     public void listen(String foo) {
         try {
             System.out.println("Received: " + foo);
-            template.convertAndSend(foo.toUpperCase());
+            capitalizeTemplate.convertAndSend(foo.toUpperCase());
         } catch (Exception e) {
             e.printStackTrace();
         }
